31bd29701711217bec076b5b925d854f5606c777
Abdessamad Bounasseh <abdessamad.bounasseh@uit.ac.ma>
2023-11-21 09:40:34 +0100

    first commit

---
+++ src/main/java/uit/ensak/dishwishbackend/DishWishBackendApplication.java
@@ -0,0 +1,13 @@
+package uit.ensak.dishwishbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishWishBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DishWishBackendApplication.class, args);
+	}
+
+}
