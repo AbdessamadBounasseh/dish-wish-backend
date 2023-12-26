142efa869042a41ed83f7451822490a988545516
Meryem EH <elhassounimeryem@outlook.fr>
2023-12-26 21:32:53 +0100

    token repo update

--- src/main/java/uit/ensak/dishwishbackend/repository/TokenRepository.java
+++ src/main/java/uit/ensak/dishwishbackend/repository/TokenRepository.java
@@ -11,16 +11,16 @@ public interface TokenRepository extends JpaRepository<VerificationToken, Long> 
 
     void deleteByToken(String token);
 
-    Optional<VerificationToken> findByCode(String code);
-
-    Optional<VerificationToken> findByToken(String token);
-
     @Query(value = """
       select t from VerificationToken t inner join Client u\s
       on t.client.id = u.id\s
       where u.id = :id and (t.expired = false or t.revoked = false)\s
       """)
     List<VerificationToken> findAllValidTokenByUserId(Long id);
+
+    Optional<VerificationToken> findByCode(String code);
+
+    Optional<VerificationToken> findByToken(String token);
 
     void deleteAllByClientId(Long id);
 
