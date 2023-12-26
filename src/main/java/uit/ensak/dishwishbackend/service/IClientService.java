626033a75690cc1110670ad62fe58235c733ac04
Meryem EH <elhassounimeryem@outlook.fr>
2023-12-26 21:45:04 +0100

    bug fix

--- src/main/java/uit/ensak/dishwishbackend/service/IClientService.java
+++ src/main/java/uit/ensak/dishwishbackend/service/IClientService.java
@@ -9,13 +9,13 @@ public interface IClientService {
 
     Client getClientById(Long id) throws ClientNotFoundException;
 
-    Client getClientByEmail(String email);
-
-    List<Client> getAllClients();
-
     void saveUserVerificationToken(Client client, String token);
 
     Client saveClient(Client client);
 
     void revokeAllUserTokens(Client client);
+
+    Client getClientByEmail(String email);
+
+    List<Client> getAllClients();
 }
