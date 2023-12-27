da4adca964ea1f1660847e4da76eb01dfb24d06a
Meryem EH <elhassounimeryem@outlook.fr>
2023-12-27 13:14:01 +0100

    created separate clientrating components

--- src/main/java/uit/ensak/dishwishbackend/service/IRatingService.java
+++
@@ -1,9 +0,0 @@
-package uit.ensak.dishwishbackend.service;
-
-import uit.ensak.dishwishbackend.dto.ClientDTO;
-
-public interface IRatingService {
-    double getClientRatings(Long clientId);
-
-    ClientDTO addRatingToClient(Long clientId, Long chefId, double rating);
-}
