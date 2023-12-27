a548fe09b5d020c3bcc4850208423b14789a309a
Meryem EH <elhassounimeryem@outlook.fr>
2023-12-27 19:42:19 +0100

    added reset-pwd endpoint

--- src/main/java/uit/ensak/dishwishbackend/controller/auth/PasswordController.java
+++ src/main/java/uit/ensak/dishwishbackend/controller/auth/PasswordController.java
@@ -5,14 +5,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uit.ensak.dishwishbackend.controller.auth.payloads.ChangePasswordRequest;
+import uit.ensak.dishwishbackend.controller.auth.payloads.ResetPasswordRequest;
+import uit.ensak.dishwishbackend.exception.ClientNotFoundException;
+import uit.ensak.dishwishbackend.exception.InvalidResetTokenException;
 import uit.ensak.dishwishbackend.service.PasswordService;
 
 import java.io.UnsupportedEncodingException;
 import java.security.Principal;
 
-@Slf4j
 @RestController
 @RequestMapping("/password")
+@Slf4j
 public class PasswordController {
 
     private final PasswordService passwordService;
@@ -21,7 +24,7 @@ public class PasswordController {
         this.passwordService = passwordService;
     }
 
-    @PatchMapping
+    @PatchMapping("/change-password")
     public ResponseEntity<?> changePassword(@RequestBody ChangePasswordRequest passwordRequest,
                                             Principal connectedUser){
         passwordService.changePassword(passwordRequest, connectedUser);
@@ -29,8 +32,17 @@ public class PasswordController {
     }
 
     @PutMapping("/forgot-password")
-    public ResponseEntity<String> forgotPassword(@RequestParam String email) throws MessagingException, UnsupportedEncodingException {
+    public ResponseEntity<String> forgotPassword(@RequestParam String email) throws MessagingException, UnsupportedEncodingException, ClientNotFoundException {
         passwordService.forgotPassword(email);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/reset-password")
+    public ResponseEntity<?> resetPassword(@RequestBody ResetPasswordRequest passwordRequest, @RequestParam String code) throws InvalidResetTokenException {
+        if (code.isBlank()){
+            throw new IllegalArgumentException("Code can't be blank.");
+        }
+        passwordService.resetPassword(passwordRequest, code);
         return ResponseEntity.ok().build();
     }
 }
