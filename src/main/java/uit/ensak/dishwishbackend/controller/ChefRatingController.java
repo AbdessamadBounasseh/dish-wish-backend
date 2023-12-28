70a7b60d5b57f5e3d8e9010620ad1ee1f9359a4d
Meryem EH <elhassounimeryem@outlook.fr>
2023-12-28 18:28:25 +0100

    added chef rating controller endpoints

--- src/main/java/uit/ensak/dishwishbackend/controller/ChefRatingController.java
+++ src/main/java/uit/ensak/dishwishbackend/controller/ChefRatingController.java
@@ -4,8 +4,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uit.ensak.dishwishbackend.dto.RatingDTO;
 import uit.ensak.dishwishbackend.exception.ClientNotFoundException;
+import uit.ensak.dishwishbackend.model.ChefRating;
 import uit.ensak.dishwishbackend.model.ClientRating;
 import uit.ensak.dishwishbackend.model.Rating;
+import uit.ensak.dishwishbackend.service.ChefRatingService;
 import uit.ensak.dishwishbackend.service.ClientRatingService;
 
 import java.util.List;
@@ -13,20 +15,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/chef-ratings")
 public class ChefRatingController {
-    private final ClientRatingService clientRatingService;
+    private final ChefRatingService chefRatingService;
 
-    public ChefRatingController(ClientRatingService clientRatingService) {
-        this.clientRatingService = clientRatingService;
+    public ChefRatingController(ChefRatingService chefRatingService) {
+        this.chefRatingService = chefRatingService;
     }
 
-    @GetMapping("/{clientId}")
-    public ResponseEntity<List<Rating>> getClientRatings(@PathVariable Long clientId) throws ClientNotFoundException {
-        List<Rating> ratings = clientRatingService.getClientRatings(clientId);
+    @GetMapping("/{chefId}")
+    public ResponseEntity<List<Rating>> getChefRatings(@PathVariable Long chefId) throws ClientNotFoundException {
+        List<Rating> ratings = chefRatingService.getChefRatings(chefId);
         return ResponseEntity.ok(ratings);
     }
 
     @PostMapping("/rate")
-    public ClientRating rateClient(@RequestBody RatingDTO ratingDetails) throws ClientNotFoundException {
-        return clientRatingService.addRatingToClient(ratingDetails);
+    public ChefRating rateChef(@RequestBody RatingDTO ratingDetails) throws ClientNotFoundException {
+        return chefRatingService.addRatingToChef(ratingDetails);
     }
 }
