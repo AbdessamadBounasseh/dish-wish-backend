26e0d749528e20f1631cdb179885a51175f38a80
NashOmega <bernardalphaomegadekpe@gmail.com>
2023-12-29 20:40:53 +0100

    create filter by name for chef and tests for linked repositories and services methods

--- src/main/java/uit/ensak/dishwishbackend/repository/ChefRepository.java
+++ src/main/java/uit/ensak/dishwishbackend/repository/ChefRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uit.ensak.dishwishbackend.model.Chef;
 
+import java.util.List;
+
 @Repository
 public interface ChefRepository extends JpaRepository<Chef, Long> {
+    List<Chef> findByFirstNameContaining(String query);
+    List<Chef> findByLastNameContaining(String query);
 }
---
+++ src/test/java/uit/ensak/dishwishbackend/repository/ChefRepositoryTest.java
@@ -0,0 +1,61 @@
+package uit.ensak.dishwishbackend.repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import uit.ensak.dishwishbackend.model.Chef;
+
+import java.util.List;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+public class ChefRepositoryTest {
+    private final ChefRepository chefRepository;
+
+    @Autowired
+    public  ChefRepositoryTest (ChefRepository chefRepository) {
+        this.chefRepository = chefRepository;
+    }
+    @Test
+    public void ChefRepository_FindByFirstNameContaining_ReturnListOfChef(){
+        Chef chef1 = new Chef();
+        Chef chef2 = new Chef();
+        chef1.setId(1L);
+        chef2.setId(2L);
+        chef1.setFirstName("abel");
+        chef2.setFirstName("abella");
+
+        chefRepository.save(chef1);
+        chefRepository.save(chef2);
+
+
+        List<Chef> returnChefs = chefRepository.findByFirstNameContaining("abel");
+        System.out.println(returnChefs);
+
+        Assertions.assertNotNull(returnChefs);
+        Assertions.assertEquals(2, returnChefs.size());
+    }
+
+    @Test
+    public void ChefRepository_FindByLastNameContaining_ReturnListOfChef(){
+        Chef chef1 = new Chef();
+        Chef chef2 = new Chef();
+        chef1.setId(1L);
+        chef2.setId(2L);
+        chef1.setLastName("fay");
+        chef2.setLastName("faycal");
+
+        chefRepository.save(chef1);
+        chefRepository.save(chef2);
+
+
+        List<Chef> returnChefs = chefRepository.findByLastNameContaining("fay");
+        System.out.println(returnChefs);
+
+        Assertions.assertNotNull(returnChefs);
+        Assertions.assertEquals(2, returnChefs.size());
+    }
+}
