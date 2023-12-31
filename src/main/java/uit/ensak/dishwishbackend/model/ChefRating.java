31852fe4b8fc504ff2cc1b6eea55ffb5052352e4
Meryem EH <elhassounimeryem@outlook.fr>
2023-12-31 05:44:41 +0100

    added rating repository test

--- src/main/java/uit/ensak/dishwishbackend/model/ChefRating.java
+++ src/main/java/uit/ensak/dishwishbackend/model/ChefRating.java
@@ -2,14 +2,12 @@ package uit.ensak.dishwishbackend.model;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 @DiscriminatorValue("CHEF_RATING")
 public class ChefRating extends Rating {
 
